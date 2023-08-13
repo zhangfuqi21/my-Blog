63a669abec489377799a715e44c483c35aad5c7b
zhangfuqi21 <zfqzcn@yeah.net>
2023-08-13 20:23:42 +0800

    1

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,24 +2,19 @@
 import React, { memo } from "react";
 import { HomeWarpper } from "./style";
 import CardItem from "./CardItem/CardItem";
-import { getBlog } from "@/api/blog";
-
+async function getBlogs() {
+  const res = await fetch("https://lancao.usemock.com/blog/blogs");
+  return res.json();
+}
 const page: React.FC = memo(async () => {
   const { data } = await getBlogs();
-  console.log(data);
-
   return (
     <HomeWarpper>
-      {data.data.list.map((item: any, index: number) => {
+      {data.list.map((item: any, index: number) => {
         return <CardItem list={item} key={index} />;
       })}
     </HomeWarpper>
   );
 });
-export async function getBlogs() {
-  const res = await getBlog();
-
-  return res;
-}
 page.displayName = "page";
 export default page;
