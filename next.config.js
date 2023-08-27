8da12fca114869d53574b8d65e0230f293b18180
zhangfuqi21 <zfqzcn@yeah.net>
2023-08-27 17:55:55 +0800

    1

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 
 const nextConfig = {
   output: "export",
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       destination: "/home",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
 };
 
 module.exports = nextConfig;
